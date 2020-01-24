fd010075fc18e80bf2cd1adb524dd8b573027254
MarkianPylat23 <marek.pt23@gmail.com>
2020-01-24 21:20:10 +0200

    add smth

---
+++ src/com.company.practise/app.runner/AppRunner.java
@@ -0,0 +1,9 @@
+package com.company.practise.app.runner;
+
+import com.company.practise.maps.Map;
+
+public class AppRunner {
+    public void runApp(){
+        new Map().playMap();
+    }
+}
---
+++ src/com.company.practise/maps/Map.java
@@ -0,0 +1,22 @@
+package com.company.practise.maps;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Map {
+    public void playMap(){
+        List<String> namesList = new ArrayList<>();
+
+        namesList.add("Marik");
+        namesList.add("Andrey");
+        namesList.add("Oleg");
+        namesList.add("Yuliyana");
+        namesList.add("John");
+
+        System.out.println(namesList);
+
+        namesList.remove("John");
+
+        System.out.println(namesList);
+    }
+}
