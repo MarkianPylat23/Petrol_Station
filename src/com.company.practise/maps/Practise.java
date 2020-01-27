1f22264452aeba8019f0a72d8dbdae33aa549d61
MarkianPylat23 <marek.pt23@gmail.com>
2020-01-27 17:44:04 +0200

    add smth

--- src/com.company.practise/maps/Practise.java
+++ src/com.company.practise/maps/Practise.java
@@ -10,5 +10,15 @@ public class Practise {
         myHashMap.put("Marik","Yulyana");
         myHashMap.put("Yulik","Zurek");
         myHashMap.put("Mesha","Lesyk");
+
+        System.out.println(myHashMap);
+
+        if (myHashMap.containsValue("Yulyana")){
+            System.out.println("Yulyana is present");
+        }
+
+        for (String key : myHashMap.values()){
+            System.out.println("Value: " + myHashMap.get("Marik"));
+        }
     }
 }
