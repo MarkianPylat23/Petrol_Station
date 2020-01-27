ef5053c85b0bb631a571c5d2f40bf9e3f62e145a
MarkianPylat23 <marek.pt23@gmail.com>
2020-01-27 14:28:12 +0200

    add smth

---
+++ src/com.company.practise/maps/Practise.java
@@ -0,0 +1,14 @@
+package com.company.practise.maps;
+
+import java.util.HashMap;
+
+public class Practise {
+    public void tryHashMap() {
+        HashMap<String, String> myHashMap = new HashMap<String, String>();
+
+        myHashMap.size();
+        myHashMap.put("Marik","Yulyana");
+        myHashMap.put("Yulik","Zurek");
+        myHashMap.put("Mesha","Lesyk");
+    }
+}
