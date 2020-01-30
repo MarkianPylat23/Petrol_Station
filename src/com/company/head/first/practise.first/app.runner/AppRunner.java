490b544dbdacf8cafe22ab694d5d56137a89145a
MarkianPylat23 <marek.pt23@gmail.com>
2020-01-30 21:06:10 +0200

    add dog

--- src/com/company/head/first/practise.first/app.runner/AppRunner.java
+++ src/com/company/head/first/practise.first/app.runner/AppRunner.java
@@ -1,11 +1,14 @@
 package com.company.head.first.practise.first.app.runner;
 
+import com.company.head.first.practise.first.programs.Dog;
 import com.company.head.first.practise.first.programs.DooBee;
 import com.company.head.first.practise.first.programs.Shuffle1;
 
 public class AppRunner {
     public void runApp(){
 //        new DooBee().calculate();
-        new Shuffle1().startShuffle();
+//        new Shuffle1().startShuffle();
+        Dog sheila = new Dog();
+        sheila.bark();
     }
 }
---
+++ src/com/company/head/first/practise.first/programs/Dog.java
@@ -0,0 +1,10 @@
+package com.company.head.first.practise.first.programs;
+
+public class Dog {
+    public void bark(){
+        int size = 40;
+        String breed;
+        String name;
+        System.out.println("BARK BARK");
+    }
+}
